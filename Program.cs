eedc1104f1bc8e6d8da206eb57ee00611370deda
Isaque-Gomes <116127894+Isaque-Gomes@users.noreply.github.com>
2023-02-06 21:04:27 -0300

    [M1S01] Ex. 1 - Entrevista
    [Variáveis]
    Faça um programa que pergunte e colete Nome, Sobrenome, Idade e Cor favorita, do usuário, e no final exiba as respostas do usuário.
    
    Signed-off-by: Isaque-Gomes <116127894+Isaque-Gomes@users.noreply.github.com>

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Perguntar e coletar o nome, sobrenome, idade e cor favorita do usuário, exibindo as respostas no final
+string nome;
+string sobrenome;
+int idade;
+string corFavorita;
+
+Console.WriteLine("Olá, tudo bem? Seja bem vindo! Vamos fazer um breve cadastro na nossa loja!");
+Console.WriteLine("Para iniciar, por favor, digite qual é o seu nome?");
+nome = Console.ReadLine();
+Console.WriteLine("E qual é o seu sobrenome?");
+sobrenome = Console.ReadLine();
+Console.WriteLine("Olá " + (nome) + " " + (sobrenome) + ", digite agora sua idade somente com números");
+idade = int.Parse(Console.ReadLine());
+Console.WriteLine("Estamos quase terminando. Por favor, agora digite sua cor favorita!");
+corFavorita = Console.ReadLine();
+Console.WriteLine("Agora só conferir os dados que preencheu! Muito obrigado!");
+Console.WriteLine($"Seu nome completo: {nome} {sobrenome}");
+Console.WriteLine("Sua idade é: " + (idade));
+Console.WriteLine("Sua cor favorita é: " + (corFavorita));
